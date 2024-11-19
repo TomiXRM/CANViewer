3f6df2e8eb279d57468919129c95c67b9b3121f2
tmxrrrm <chocolate0785lego@icloud.com>
2024-11-20 02:47:17 +0900

    fix typo

--- src/utils/validator.py
+++ src/utils/validator.py
@@ -1,4 +1,4 @@
-from PiSide6.QtGui import QIntValidator, QRegularExpressionValidator
+from PySide6.QtGui import QIntValidator, QRegularExpressionValidator
 from PySide6.QtCore import (QRegularExpression,Qt)
 
 hex_validator = QRegularExpressionValidator(QRegularExpression("^[0-9A-Fa-f]+$"))  # HEX
