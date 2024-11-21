7c879c48369c879a84041d60bcea93a57e56c74c
tomixrm <chocolate0785lego@icloud.com>
2024-11-21 15:49:01 +0900

    バリデーション関連の関数を追加

--- src/utils/validator.py
+++ src/utils/validator.py
@@ -1,12 +1,13 @@
 from PySide6.QtCore import QRegularExpression
 from PySide6.QtGui import QIntValidator, QRegularExpressionValidator
+from PySide6.QtWidgets import QLineEdit
 
 
 class Validator:
-    hex_validator = QRegularExpressionValidator(
-        QRegularExpression("^[0-9A-Fa-f]+$")
-    )  # HEX
-    dec_validator = QIntValidator()  # DEC
+    # HEX
+    hex_validator = QRegularExpressionValidator(QRegularExpression("^[0-9A-Fa-f]+$"))
+    # DEC
+    dec_validator = QIntValidator()
 
     @staticmethod
     def decimalize(value_str: str = "", radix_type="dec") -> int:
@@ -16,5 +17,23 @@ class Validator:
             value = int(value_str.replace(",", "").strip())
         return value
 
-    # @staticmethod
-    # def decimalize_hex_to_str
+    @staticmethod
+    def text_decimalize_from_hex_text(self, hex_text: str) -> str:
+        dec_value: int = self.decimalize(hex_text)
+        hex_value_text: str = hex(dec_value)[2:].upper()
+        return hex_value_text
+
+    @staticmethod
+    def text_hexadecimalize_from_decimal_text(self, dec_text: str) -> str:
+        hex_value: int = int(dec_text, 16)
+        dec_value_text: str = str(hex_value)
+        return dec_value_text
+
+    @staticmethod
+    def assign_style_on_text_edit_with_radix(
+        text_edit: QLineEdit, radix_type: str
+    ) -> None:
+        if radix_type == "dec":
+            pass
+        elif radix_type == "hex":
+            pass
